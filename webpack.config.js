dbb503757635edfecd3347449efb6ba945950246
JeongWoo <fixingwoo@gmail.com>
2023-02-12 00:58:48 +0900

    feat: Add various loaders (css, style, file, url)

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,31 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        use: [path.resolve("./my-webpack-loader.js")],
+        test: /\.css$/, // .css 확장자로 끝나는 모든 파일
+        use: ["style-loader", "css-loader"], // css-loader를 적용
+      },
+      {
+        test: /\.png|.jpg|.gif|.svg$/,
+        use: [
+          {
+            loader: "file-loader",
+            query: {
+              publicPath: "./dist/",
+              name: "[name].[ext]?[hash]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.png$/,
+        use: {
+          loader: "url-loader", // url 로더를 설정한다
+          options: {
+            publicPath: "./dist/", // file-loader와 동일
+            name: "[name].[ext]?[hash]", // file-loader와 동일
+            limit: 5000, // 5kb 미만 파일만 data url로 처리
+          },
+        },
       },
     ],
   },
