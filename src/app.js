08c9be5bab5430977eab471d9447f2d3ea4097ae
JeongWoo <fixingwoo@gmail.com>
2023-02-12 23:18:10 +0900

    feat: 여러가지 플러그인 학습

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
 import "./app.css";
+
+console.log(TWO);
+console.log(api.domain);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const path = require("path");
+const webpack = require("webpack");
+const childProcess = require("child_process");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -13,7 +18,12 @@ module.exports = {
     rules: [
       {
         test: /\.css$/, // .css 확장자로 끝나는 모든 파일
-        use: ["style-loader", "css-loader"], // css-loader를 적용
+        use: [
+          process.env.NODE_ENV === "production"
+            ? MiniCssExtractPlugin.loader
+            : "style-loader",
+          "css-loader",
+        ], // css-loader를 적용
       },
       {
         test: /\.png|.jpg|.gif|.svg$/,
@@ -21,7 +31,7 @@ module.exports = {
           {
             loader: "file-loader",
             query: {
-              publicPath: "./dist/",
+              // publicPath: "./dist/",
               name: "[name].[ext]?[hash]",
             },
           },
@@ -32,7 +42,7 @@ module.exports = {
         use: {
           loader: "url-loader", // url 로더를 설정한다
           options: {
-            publicPath: "./dist/", // file-loader와 동일
+            // publicPath: "./dist/", // file-loader와 동일
             name: "[name].[ext]?[hash]", // file-loader와 동일
             limit: 5000, // 5kb 미만 파일만 data url로 처리
           },
@@ -40,4 +50,34 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `
+        Build Date: ${new Date().toLocaleString()}
+        Git Version: ${childProcess.execSync("git rev-parse --short HEAD")}
+        Author: ${childProcess.execSync("git config user.name")}
+      `,
+    }),
+    new webpack.DefinePlugin({
+      TWO: JSON.stringify("1+1"),
+      "api.domain": JSON.stringify("http://dev.api.domain.com"),
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      templateParameters: {
+        env: process.env.NODE_ENV === "development" ? "개발" : "",
+      },
+      minify:
+        process.env.NODE_ENV === "production"
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+            }
+          : false,
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
 };
